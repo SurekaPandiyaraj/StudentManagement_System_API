837dca0b0704a7a7aaef27e4323353b8a4599260
Prem Arththy <arththyprem@gmail.com>
2024-11-18 21:40:59 +0530

    user setup

---
+++ StudentManagement_System_API/Repository/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using StudentManagement_System_API.Database;
+using StudentManagement_System_API.IRepository;
+
+namespace StudentManagement_System_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly StudentManagementContext _studentManagementContext;
+
+        public UserRepository(StudentManagementContext studentManagementContext)
+        {
+            _studentManagementContext = studentManagementContext;
+        }
+    }
+}
---
+++ StudentManagement_System_API/Service/UserService.cs
@@ -0,0 +1,15 @@
+﻿using StudentManagement_System_API.IRepository;
+using StudentManagement_System_API.IService;
+
+namespace StudentManagement_System_API.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}

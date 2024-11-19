0a41ab9de8d3590ae244bb63aa2b9ec34527a40b
UT01188 <UT01188@UNICOMTIC5>
2024-11-19 15:51:37 +0530

    update exam controllers

--- StudentManagement_System_API/Controllers/ExamControllers.cs
+++ StudentManagement_System_API/Controllers/ExamControllers.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagement_System_API.Entity;
 using StudentManagement_System_API.IService;
 using StudentManagement_System_API.Service;
 
@@ -14,6 +15,73 @@ namespace StudentManagement_System_API.Controllers
         public ExamControllers(ExamService examService)
         {
             _examService = examService;
+        }
+
+
+        // GET: api/exams
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Exam>>> GetAll()
+        {
+            var exams = await _examService.GetAllAsync();
+            return Ok(exams);
+        }
+
+        // GET: api/exams/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exam>> GetById(int id)
+        {
+            var exam = await _examService.GetByIdAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return Ok(exam);
+        }
+
+        // POST: api/exams
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Exam exam)
+        {
+            if (exam == null)
+            {
+                return BadRequest();
+            }
+
+            await _examService.AddAsync(exam);
+            return CreatedAtAction(nameof(GetById), new { id = exam.Id }, exam);
+        }
+
+        // PUT: api/exams/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Exam exam)
+        {
+            if (exam == null || exam.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var existingExam = await _examService.GetByIdAsync(id);
+            if (existingExam == null)
+            {
+                return NotFound();
+            }
+
+            await _examService.UpdateAsync(exam);
+            return NoContent();
+        }
+
+        // DELETE: api/exams/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var exam = await _examService.GetByIdAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            await _examService.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
